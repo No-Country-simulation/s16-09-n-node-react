e099e2ca2b48d8e8d9fb5629ea0a71dbd042db19
Aricoins <arielgarcia79@gmail.com>
2024-07-10 20:50:58 -0300

    feat: registro y login con clerk

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,16 +3,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+
 import { store } from "./Redux/store";
 import { Provider } from "react-redux";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 import axios from 'axios'
 axios.defaults.baseURL = 'localhost:3000/'
 
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
     <Provider store={store} >
       <App />
     </Provider>
+    </ClerkProvider>
   </React.StrictMode>,
 )
