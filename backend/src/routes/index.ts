4ca6cb4c369c3f3fe56084a46856d5a09af2b5a1
Edu Moreno <e.m.morenolp@gmail.com>
2024-07-10 23:34:33 -0300

    implementacion de autentificacion y rutas login register y logout

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,8 +3,11 @@ import { Router } from 'express';
 import { homeRouter } from './home.routes';
 import { userRouter } from './user.routes';
 import { calendarRouter } from './calendar.routes'
+import { authRouter } from './auth.routes';
 
 export const mainRouter = Router();
+
 mainRouter.use('/', homeRouter);
 mainRouter.use('/user', userRouter);
 mainRouter.use('/calendar/v3', calendarRouter);
+mainRouter.use('/auth', authRouter);
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 
 import { createUser, getAllUsers } from '@/controllers/userController';
+import { requireAuth } from '@/middleware/authMiddleware';
 
 export const userRouter = Router();
 
-userRouter.get('/getAllUsers', getAllUsers);
+userRouter.get('/getAllUsers', requireAuth, getAllUsers);
 userRouter.post('/createUser', createUser);
