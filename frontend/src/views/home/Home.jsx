3b05aee95c317a7348f17b7aa3bcba24cbb9c85e
Pablo Spector <pablo_spector@hotmail.com>
2024-07-15 19:35:22 -0300

    landing en home view

--- frontend/src/views/home/Home.jsx
+++ frontend/src/views/home/Home.jsx
@@ -1,14 +1,35 @@
 import './home.css'
-import Navbar from "../../components/commons/navbar/Navbar";
-import Footer from "../../components/commons/footer/Footer";
-import { Outlet } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div className='home-container'>
-      <Navbar />
-      <Outlet />
-      <Footer />
+      <div className="home-header">
+        <img src="/assets/logo-brand.svg" alt="Proyect Brand" className='home-brand' />
+        <div className="home-auth-container">
+          <NavLink to='/auth/sign-in' className='home-navlinks'>Iniciar Sesión</NavLink>
+          <NavLink to='/auth/sign-up' className='home-navlinks'>Registrarse</NavLink>
+        </div>
+      </div>
+      <div className="home-main">
+        <div className="home-container1">
+          <img src="/assets/rocket-landing.svg" alt="rocket" className='home-rocket-image' />
+          <div className="home-text-container">
+            <span className='home-text'>
+              Convierte la gestión de tus proyectos en una experiencia
+            </span>
+            <span className='home-text-green'>
+              rápida, eficiente y colaborativa
+            </span>
+            <NavLink className='home-cta-link'>Conoce más</NavLink>
+          </div>
+        </div>
+        <div className="home-hero-images">
+          <img src="/assets/device-mac.svg" alt="mac device image" className='home-mac-device-image' />
+          <img src="/assets/device-phone.svg" alt="phone device image" className='home-device-image' />
+        </div>
+      </div>
+      <div className="home-flint"></div>
     </div>
   )
 }
