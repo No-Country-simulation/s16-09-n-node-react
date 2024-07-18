08cfa0ee876aadac29d9f10395297c762ad8eb67
Aricoins <arielgarcia79@gmail.com>
2024-07-17 21:08:47 -0300

    mas estilos para el form

--- frontend/src/components/auth/sign-in/SignIn.jsx
+++ frontend/src/components/auth/sign-in/SignIn.jsx
@@ -1,8 +1,39 @@
 import { SignIn } from "@clerk/clerk-react";
 import logo from '../../../assets/logo.svg';
 import icono from '../../../assets/icono.svg';
+import { useEffect } from 'react';
 
 export default function SignInPage() {
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const header = document.querySelector('.cl-headerTitle');
+      if (header) {
+        header.textContent = 'Bienvenidos, ';
+        header.style.color = 'black';
+        header.style.display = 'flex';
+        header.style.justifyContent= 'flex-start'; 
+
+        const subtitle = document.querySelector('.cl-headerSubtitle');
+        if (subtitle) {
+          subtitle.remove();
+        }
+        const dividerText = document.querySelector('.cl-dividerText');
+        if (dividerText) {
+          dividerText.innerHTML = `<div class="cl-container">
+                                    <div class="cl-"></div>
+                                    <span>-------------------------------------   o   -----------------------------------</span>
+                                    <div class="cl-dividerText"></div>
+                                   </div>`;
+          clearInterval(intervalId);
+        }
+
+        clearInterval(intervalId);
+      }
+    }, 100); // revisa cada 100ms
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="mt-40 flex flex-col-reverse md:flex-row md:mt-0 items-center w-screen h-screen">
       <div className="flex-1 flex justify-center items-center">
