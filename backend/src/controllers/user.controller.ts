479ab2f8e791e9a4bbfd6ee57211616616f99643
Edu Moreno <e.m.morenolp@gmail.com>
2024-07-25 19:21:06 -0300

    modificaciones en migrations errores. de prisma

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -13,14 +13,17 @@ export const getUsers = async (req: Request, res: Response) => {
     const users = await prisma.user.findMany({
       select: {
         id: true,
+        username: true,
         email: true,
         name: true,
         lastName: true,
         createdAt: true,
       },
     });
-    res.json(users);
+
+    res.status(200).json(users);
   } catch (error) {
+    console.error('Error fetching users:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
