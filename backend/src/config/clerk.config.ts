dcc163f606b12ec8d8400b7b732034dacc1148cd
Jorge Trujillo <39315478+xiriuxb@users.noreply.github.com>
2024-07-29 20:56:46 -0500

    Back: deleted unused clerk files

--- backend/src/config/clerk.config.ts
+++
@@ -1,7 +0,0 @@
-import { createClerkClient } from '@clerk/clerk-sdk-node';
-
-const clerk = createClerkClient({
-  secretKey: 'sk_test_QlGnZWzkWjXl3ORfeqmHbhcUKLeX5dekKVuBzJourb',
-});
-
-export default clerk;
